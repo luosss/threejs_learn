29b2707025c8207f1110fe1cc7cb0c264981ec9d
luoshao <977763419@qq.com>
2022-05-01 18:23:14 +0800

    开发环境准备

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>
+    hello three.jssfsdfsdf
+  </h1>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+
+<style>
+
+</style>
